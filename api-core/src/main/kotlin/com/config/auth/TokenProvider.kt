f12485ef2c80ceb0d078a9cd5048b0a2e8932d05
devforlove <inwookgusa@gmail.com>
2025-08-07 22:52:43 +0900

    chore: 알람 모듈 분리

---
+++ api-core/src/main/kotlin/com/config/auth/TokenProvider.kt
@@ -0,0 +1,85 @@
+package com.senok.config.auth
+
+import io.jsonwebtoken.*
+import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.security.SecurityException
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.stereotype.Component
+import java.util.*
+import javax.crypto.SecretKey
+
+@Component
+class TokenProvider {
+
+    @Value("{jwt.key}")
+    private lateinit var key: String
+    private lateinit var secretKey: SecretKey
+
+    companion object {
+        private val ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L
+        private val REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L * 24 * 7
+        private val KEY_ROLE = "role"
+    }
+
+    @PostConstruct
+    private fun init() {
+        secretKey = Keys.hmacShaKeyFor(key.toByteArray())
+        println(secretKey)
+    }
+
+    fun generateAccessToken(authentication: Authentication): String {
+        return generateToken(authentication)
+    }
+
+    fun getAuthentication(token: String): Authentication {
+        val claims = parseClaims(token)
+        val authorities = getAuthorities(claims)
+
+
+        return UsernamePasswordAuthenticationToken()
+    }
+
+    fun getAuthorities(claims: Claims): List<GrantedAuthority> {
+        return listOf(
+            SimpleGrantedAuthority(claims[KEY_ROLE].toString())
+        )
+    }
+
+    private fun generateToken(authentication: Authentication): String {
+        val now = Date()
+        val expireTime = Date(now.time + ACCESS_TOKEN_EXPIRE_TIME)
+
+        val authorities = authentication.authorities
+            .map { authority -> authority.authority }
+            .joinToString { it.toString() }
+
+        return Jwts.builder()
+            .setSubject(authentication.name)
+            .claim(KEY_ROLE, authorities)
+            .setIssuedAt(now)
+            .setExpiration(expireTime)
+            .signWith(secretKey, SignatureAlgorithm.HS512)
+            .compact()
+    }
+
+    private fun parseClaims(token: String): Claims {
+        return try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).body
+        } catch (e : ExpiredJwtException) {
+            return e.claims
+        } catch (e : MalformedJwtException) {
+            // TODO(토큰 예외 정리)
+            throw Exception("JWT token expired", e)
+        } catch (e : SecurityException) {
+            // TODO(토큰 예외 정리)
+            throw Exception("JWT token expired", e)
+        } catch (e: Exception) {
+            throw Exception("unknown issue", e)
+        }
+    }
+}
