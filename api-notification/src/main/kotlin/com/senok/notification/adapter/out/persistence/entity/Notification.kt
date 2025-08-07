a993f5b345a1c9735784a93711c906deebff2d97
devforlove <inwookgusa@gmail.com>
2025-08-08 00:23:22 +0900

    chore: core-web, core-db 모듈 분리

--- api-notification/src/main/kotlin/com/senok/notification/adapter/out/persistence/entity/Notification.kt
+++ api-notification/src/main/kotlin/com/senok/notification/adapter/out/persistence/entity/Notification.kt
@@ -1,5 +1,8 @@
-package com.senok.alarm.adapter.out.persistence.entity
+package com.senok.notification.adapter.out.persistence.entity
 
+
+import com.senok.alarm.adapter.out.persistence.entity.NotificationStatus
+import com.senok.alarm.adapter.out.persistence.entity.NotificationType
 import com.senok.common.db.entitty.BaseEntity
 import com.senok.common.db.entitty.EntityId
 import jakarta.persistence.*
